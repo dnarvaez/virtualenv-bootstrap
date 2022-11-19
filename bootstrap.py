9e9696a72fe98478d017d07507c838311a7abb55
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-19 12:29:50 +0000

    Adding tarfile member sanitization to extractall()

--- bootstrap.py
+++ bootstrap.py
@@ -65,7 +65,26 @@ def create_virtualenv():
         f = urllib.request.urlopen(url)
 
         with tarfile.open(fileobj=f, mode="r:gz") as tar:
-            tar.extractall(get_cache_dir())
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, get_cache_dir())
 
     subprocess.check_call(["python3",
                            os.path.join(source_dir, "virtualenv.py"),
